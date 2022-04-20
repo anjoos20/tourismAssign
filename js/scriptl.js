cadaa31708618916c72ed1926222ec185601320b
Anju Balakrishnan <anjoos2006@gmail.com>
2022-04-20 20:35:19 +0530

    Assignment1 - phase1 - validation of Email, password and phone number

--- js/scriptl.js
+++ js/scriptl.js
@@ -2,12 +2,10 @@ function validate(){
     console.log("Inside validation");
     let email = document.getElementById("form3Example3");
     let errore = document.getElementById("errore");
-       let pwd = document.getElementById("form3Example4");
-       let errorp = document.getElementById("errorp");
-       let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    let pwd = document.getElementById("form3Example4");
+    let errorp = document.getElementById("errorp");
+    let regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
     //    let regex = /(?=^.{8,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
-       let strong = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
-       let medium = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
        var emailok = new Boolean(false);
        var pwdok = new Boolean(false);
     if (regexp.test(email.value)){
@@ -20,18 +18,14 @@ function validate(){
         errore.style.color = "red";
         emailok=false;
     };
-    if (strong.test(pwd.value)){
-        errorp.innerHTML = "Strong password."
+    console.log(pwd.value.length);
+    if (pwd.value.length > 7){
+        errorp.innerHTML = "Valid password."
         errorp.style.color = "green";
         pwdok = true;
     }
-    else if (medium.test(pwd.value)){
-        errorp.innerHTML = "Medium password."
-        errorp.style.color = "orange";
-        pwdok = true;  
-    }
     else{
-        errorp.innerHTML = "Weak password: Password should have both uppercase, lowercase alphabets and numerics ";
+        errorp.innerHTML = "Invalid password ";
         errorp.style.color = "red";
         pwdok = false;
     };
